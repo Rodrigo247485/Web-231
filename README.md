dda47849ab53a3d6fb05db4b39e4a798b39a4655
Rodrigo Piccardo <rodpiccardo@gmail.com>
2025-08-15 00:36:53 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Web-231
-assignments 
+# web-231
+classwork
+<h1>WEB 231 Enterprise JavaScript 1</h1>
+<h2>CONTRIBUTORS</h2>
+<ul>
+    <li>Richard Krasso </li>
+    <li>Rodrigo Piccardo</li>
+  </ul>
---
+++ week-1/Project/project01-02_txt.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author:
+    Date:
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+(service1name = "basic"),
+  (service2name = "express"),
+  (service3name = "Extreme"),
+  (service4name = "Ultimate"),
+  (service1speed = "0 Mbps"),
+  (service2speed = "100 Mbps"),
+  (service3speed = "500 Mbps"),
+  (service4speed = "1 Gig");
